4cdf38d3f8115c0c6d6b7bc4b35a2b226a27e038
Federico Pecora <federico.pecora@oru.se>
2017-10-05 17:33:30 +0200

    Added conservative multipliers in ConstantAccelerationForwardModel

--- src/main/java/se/oru/coordination/coordination_oru/ConstantAccelerationForwardModel.java
+++ src/main/java/se/oru/coordination/coordination_oru/ConstantAccelerationForwardModel.java
@@ -9,18 +9,24 @@ public class ConstantAccelerationForwardModel implements ForwardModel {
 	private double maxAccel, maxVel;
 	private int CONTROL_PERIOD = -1;
 	private double TEMPORAL_RESOLUTION = -1;
+	private int numControlPeriodsFreeRun = 1;
 
 	@Deprecated
 	public ConstantAccelerationForwardModel(double maxAccel, double maxVel) {
 		this.maxAccel = maxAccel;
 		this.maxVel = maxVel;	
 	}
-
-	public ConstantAccelerationForwardModel(double maxAccel, double maxVel, int CONTROL_PERIOD, double TEMPORAL_RESOLUTION) {
+	
+	public ConstantAccelerationForwardModel(double maxAccel, double maxVel, int CONTROL_PERIOD, double TEMPORAL_RESOLUTION, int numControlPeriodsFreeRun) {
 		this.maxAccel = maxAccel;
 		this.maxVel = maxVel;	
 		this.CONTROL_PERIOD = CONTROL_PERIOD;
 		this.TEMPORAL_RESOLUTION = TEMPORAL_RESOLUTION;
+		this.numControlPeriodsFreeRun = numControlPeriodsFreeRun;
+	}
+
+	public ConstantAccelerationForwardModel(double maxAccel, double maxVel, int CONTROL_PERIOD, double TEMPORAL_RESOLUTION) {
+		this(maxAccel,maxVel,CONTROL_PERIOD,TEMPORAL_RESOLUTION,1);
 	}
 
 	@Override
@@ -30,14 +36,14 @@ public class ConstantAccelerationForwardModel implements ForwardModel {
 		double time = 0.0;
 		double deltaTime = 0.0001;
 		if (CONTROL_PERIOD != -1) {
-			while (time*TEMPORAL_RESOLUTION < CONTROL_PERIOD) {
-				se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeTrackerRK4.integrateRK4(state, time, deltaTime, false, maxVel, 1.0, maxAccel);
+			while (time*TEMPORAL_RESOLUTION < CONTROL_PERIOD*numControlPeriodsFreeRun) {
+				se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeTrackerRK4.integrateRK4(state, time, deltaTime, false, maxVel, 1.0, maxAccel*1.1);
 				time += deltaTime;
 			}
 		}
 		while (state.getVelocity() > 0) {
 			if (state.getPosition() > distance) return false;
-			se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeTrackerRK4.integrateRK4(state, time, deltaTime, true, maxVel, 1.0, maxAccel);
+			se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeTrackerRK4.integrateRK4(state, time, deltaTime, true, maxVel, 1.0, maxAccel*0.9);
 			time += deltaTime;
 		}
 		return true;
