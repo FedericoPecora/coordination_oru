6a7dc81645e18fb78c5618917a6ba4d82dea0f5e
Federico Pecora <federico.pecora@oru.se>
2018-05-11 17:15:54 +0200

    Fixed ROSCore flag bug in RVizVisualization

--- src/main/java/se/oru/coordination/coordination_oru/util/RVizVisualization.java
+++ src/main/java/se/oru/coordination/coordination_oru/util/RVizVisualization.java
@@ -122,6 +122,7 @@ public class RVizVisualization implements FleetVisualization, NodeMain {
 	public RVizVisualization(ConnectedNode node) {
 		this(false);
 		this.node = node;
+		this.ready = true;
 	}
 
 	
