ded8b79918534a570253fe6707c43cdd32d3a460
Federico Pecora <federico.pecora@gmail.com>
2020-01-14 17:14:21 +0100

    Added idle example to template

---
+++ src/main/java/se/oru/coordination/coordination_oru/examples/FourIdleRobots.java
@@ -0,0 +1,92 @@
+package se.oru.coordination.coordination_oru.examples;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Comparator;
+
+import org.metacsp.multi.spatioTemporal.paths.Pose;
+import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+import se.oru.coordination.coordination_oru.ConstantAccelerationForwardModel;
+import se.oru.coordination.coordination_oru.CriticalSection;
+import se.oru.coordination.coordination_oru.Mission;
+import se.oru.coordination.coordination_oru.RobotAtCriticalSection;
+import se.oru.coordination.coordination_oru.RobotReport;
+import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner;
+import se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeCoordinatorSimulation;
+import se.oru.coordination.coordination_oru.util.BrowserVisualization;
+import se.oru.coordination.coordination_oru.util.JTSDrawingPanelVisualization;
+
+public class FourIdleRobots {
+
+		public static void main(String[] args) throws InterruptedException {
+
+		double MAX_ACCEL = 1.0;
+		double MAX_VEL = 4.0;
+		//Instantiate a trajectory envelope coordinator.
+		//The TrajectoryEnvelopeCoordinatorSimulation implementation provides
+		// -- the factory method getNewTracker() which returns a trajectory envelope tracker
+		// -- the getCurrentTimeInMillis() method, which is used by the coordinator to keep time
+		//You still need to add one or more comparators to determine robot orderings thru critical sections (comparators are evaluated in the order in which they are added)
+		final TrajectoryEnvelopeCoordinatorSimulation tec = new TrajectoryEnvelopeCoordinatorSimulation(MAX_VEL,MAX_ACCEL);
+		tec.addComparator(new Comparator<RobotAtCriticalSection> () {
+			@Override
+			public int compare(RobotAtCriticalSection o1, RobotAtCriticalSection o2) {
+				CriticalSection cs = o1.getCriticalSection();
+				RobotReport robotReport1 = o1.getRobotReport();
+				RobotReport robotReport2 = o2.getRobotReport();
+				return ((cs.getTe1Start()-robotReport1.getPathIndex())-(cs.getTe2Start()-robotReport2.getPathIndex()));
+			}
+		});
+		tec.addComparator(new Comparator<RobotAtCriticalSection> () {
+			@Override
+			public int compare(RobotAtCriticalSection o1, RobotAtCriticalSection o2) {
+				return (o2.getRobotReport().getRobotID()-o1.getRobotReport().getRobotID());
+			}
+		});
+
+		//You probably also want to provide a non-trivial forward model
+		//(the default assumes that robots can always stop)
+		tec.setForwardModel(1, new ConstantAccelerationForwardModel(MAX_ACCEL, MAX_VEL, tec.getTemporalResolution(), tec.getControlPeriod(), tec.getTrackingPeriod()));
+		tec.setForwardModel(2, new ConstantAccelerationForwardModel(MAX_ACCEL, MAX_VEL, tec.getTemporalResolution(), tec.getControlPeriod(), tec.getTrackingPeriod()));
+		tec.setForwardModel(3, new ConstantAccelerationForwardModel(MAX_ACCEL, MAX_VEL, tec.getTemporalResolution(), tec.getControlPeriod(), tec.getTrackingPeriod()));
+		tec.setForwardModel(4, new ConstantAccelerationForwardModel(MAX_ACCEL, MAX_VEL, tec.getTemporalResolution(), tec.getControlPeriod(), tec.getTrackingPeriod()));
+
+		Coordinate footprint1 = new Coordinate(-1.0,0.5);
+		Coordinate footprint2 = new Coordinate(1.0,0.5);
+		Coordinate footprint3 = new Coordinate(1.0,-0.5);
+		Coordinate footprint4 = new Coordinate(-1.0,-0.5);
+		tec.setDefaultFootprint(footprint1, footprint2, footprint3, footprint4);
+
+		//Need to setup infrastructure that maintains the representation
+		tec.setupSolver(0, 100000000);
+
+		//Setup a simple GUI (null means empty map, otherwise provide yaml file)
+		//JTSDrawingPanelVisualization viz = new JTSDrawingPanelVisualization();
+		final BrowserVisualization viz = new BrowserVisualization();
+		viz.setInitialTransform(43, 11, 1.6);
+		tec.setVisualization(viz);
+
+		tec.setUseInternalCriticalPoints(false);
+
+		//MetaCSPLogging.setLevel(tec.getClass().getSuperclass(), Level.FINEST);
+
+		Pose startPoseRobot1 = new Pose(10.0,3.0,Math.PI);
+		Pose startPoseRobot2 = new Pose(3.0,10.0,Math.PI/2);
+		Pose startPoseRobot3 = new Pose(10.0,17.0,0.0);
+		Pose startPoseRobot4 = new Pose(17.0,10.0,-Math.PI/2);
+
+		//Place robots in their initial locations (looked up in the data file that was loaded above)
+		// -- creates a trajectory envelope for each location, representing the fact that the robot is parked
+		// -- each trajectory envelope has a path of one pose (the pose of the location)
+		// -- each trajectory envelope is the footprint of the corresponding robot in that pose
+		tec.placeRobot(1, startPoseRobot1);
+		tec.placeRobot(2, startPoseRobot2);
+		tec.placeRobot(3, startPoseRobot3);
+		tec.placeRobot(4, startPoseRobot4);
+		
+	}
+
+}
