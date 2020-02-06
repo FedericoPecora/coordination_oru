720fef6ad1486fe3f14bd3aeeaeb1902f8d9853c
Anna Mannucci <anna.mannucci90@gmail.com>
2020-02-06 14:47:04 +0100

    update

--- src/main/java/se/oru/coordination/coordination_oru/fleetmasterinterface/FleetMasterInterface.java
+++ src/main/java/se/oru/coordination/coordination_oru/fleetmasterinterface/FleetMasterInterface.java
@@ -10,18 +10,18 @@ import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
 import com.sun.jna.NativeLong;
-import com.sun.jna.ptr.PointerByReference;
 import com.vividsolutions.jts.geom.Coordinate;
 
 import se.oru.coordination.coordination_oru.CriticalSection;
+import se.oru.coordination.coordination_oru.fleetmasterinterface.FleetMasterInterfaceLib.GridParams;
 import se.oru.coordination.coordination_oru.fleetmasterinterface.FleetMasterInterfaceLib.PathPose;
+import se.oru.coordination.coordination_oru.fleetmasterinterface.FleetMasterInterfaceLib.TrajParams;
 
 
 public class FleetMasterInterface {
 	
-	private HashMap<Integer,NativeLong> paths = null; //robot, pathID
-	private HashMap<Integer,PointerByReference> trajParams = null; //FIXME robotID, trajectory parameters
-	private PointerByReference gridParams = null; //FIXME
+	private HashMap<Integer, NativeLong> paths = null; //teID, pathID
+	private HashMap<Integer, TrajParams> trajParams = null; //FIXME teID, trajectory parameters
 	
 	public static FleetMasterInterfaceLib INSTANCE = null;
 	static {
@@ -29,14 +29,29 @@ public class FleetMasterInterface {
 		INSTANCE = Native.loadLibrary("fleetmaster", FleetMasterInterfaceLib.class);
 	}
 	
-	//FIXME
-	void init(PointerByReference gridParams) {
-		INSTANCE.init(gridParams);
+	/**
+	 * 
+	 * @param origin_x
+	 * @param origin_y
+	 * @param origin_theta
+	 * @param resolution
+	 * @param width
+	 * @param height
+	 */
+	void init(double origin_x, double origin_y, double origin_theta, double resolution, long width, long height) {
+		INSTANCE.init(new GridParams(origin_x, origin_y, origin_theta, resolution, new NativeLong(width), new NativeLong(height)));
 	}
 	
-	boolean addPath(int robotID, List<PoseSteering> pathToAdd, Coordinate ... coordinates) {
+	/**
+	 * 
+	 * @param teID
+	 * @param pathToAdd
+	 * @param coordinates
+	 * @return
+	 */
+	boolean addPath(int teID, List<PoseSteering> pathToAdd, Coordinate ... coordinates) {
 		if (pathToAdd.size() == 0 || coordinates.length == 0) return false;
-		clearPath(robotID);
+		clearPath(teID);
 		
 		//Parse the Java values for the path and the footprint
 		List<PathPose> path = new ArrayList<PathPose>();
@@ -47,13 +62,17 @@ public class FleetMasterInterface {
 		for (int i = 0; i < coordinates.length; i++) footprint.add(new Pair<Double,Double>(coordinates[i].x,coordinates[i].y));
 		
 		//Call the method
-		paths.put(robotID, INSTANCE.addPath(path, trajParams.get(robotID), footprint));
+		paths.put(teID, INSTANCE.addPath(path, trajParams.get(teID), footprint));
 		return true;
 	}
 	
-	void clearPath(int robotID) {
-		if (paths.containsKey(robotID)) {
-			INSTANCE.removePath(paths.get(robotID));
+	/**
+	 * Remove the path related to the given {@link TrajectoryEnvelope} from the fleetmaster gridmap.
+	 * @param teID The ID of the trajectory envelope to be cleared.
+	 */
+	void clearPath(int teID) {
+		if (paths.containsKey(teID)) {
+			INSTANCE.removePath(paths.get(teID));
 		}
 	}
 	
@@ -63,18 +82,25 @@ public class FleetMasterInterface {
 	 * @param currentIdx The path index.
 	 * @return true if the path index has been correctly updated.
 	 */
-	boolean updateCurrentPathIdx(int robotID, int currentIdx) {
-		if (paths.containsKey(robotID)) {
-			 return INSTANCE.updateCurrentPathIdx(paths.get(robotID), new NativeLong(currentIdx));
+	boolean updateCurrentPathIdx(int teID, int currentIdx) {
+		if (paths.containsKey(teID)) {
+			 return INSTANCE.updateCurrentPathIdx(paths.get(teID), new NativeLong(currentIdx));
 		 }
 		 return false;
 	}
 	
+	/**
+	 * 
+	 * @param cs
+	 * @param pathId1TTCDelays
+	 * @param pathId2TTCDelays
+	 * @return
+	 */
 	Pair<Double,Double> queryTimeDelay(CriticalSection cs, ArrayList<Pair<NativeLong, Double>> pathId1TTCDelays, ArrayList<Pair<NativeLong, Double>> pathId2TTCDelays) {
-		int robotID1 = cs.getTe1().getRobotID();
-		int robotID2 = cs.getTe2().getRobotID();
-		if (paths.containsKey(robotID1) && (paths.containsKey(robotID2))) {
-		return INSTANCE.queryTimeDelay(paths.get(robotID1), paths.get(robotID2), 
+		int teID1 = cs.getTe1().getID();
+		int teID2 = cs.getTe2().getID();
+		if (paths.containsKey(teID1) && (paths.containsKey(teID2))) {
+		return INSTANCE.queryTimeDelay(paths.get(teID1), paths.get(teID2), 
 				new Pair<NativeLong, NativeLong>(new NativeLong(cs.getTe1Start()), new NativeLong(cs.getTe1End())), new Pair<NativeLong, NativeLong>(new NativeLong(cs.getTe2Start()), new NativeLong(cs.getTe2End())), 
 				pathId1TTCDelays, pathId2TTCDelays);
 		}
--- src/main/java/se/oru/coordination/coordination_oru/fleetmasterinterface/FleetMasterInterfaceLib.java
+++ src/main/java/se/oru/coordination/coordination_oru/fleetmasterinterface/FleetMasterInterfaceLib.java
@@ -13,9 +13,9 @@ import com.sun.jna.ptr.PointerByReference;
 
 public interface FleetMasterInterfaceLib {
 	
-	void init(PointerByReference gridParams);
+	void init(GridParams gridParams);
 	
-	NativeLong addPath(List<PathPose> path, PointerByReference trajParams, List<Pair<Double,Double>> footPrint);
+	NativeLong addPath(List<PathPose> path, TrajParams trajParams, List<Pair<Double,Double>> footPrint);
 	
     void removePath(NativeLong id);
 
@@ -25,11 +25,13 @@ public interface FleetMasterInterfaceLib {
 	
 	public static class PathPose extends Structure {
 		public static class ByReference extends PathPose implements Structure.ByReference {
-
+			public ByReference(double x, double y, double theta) {
+				super(x, y, theta);
+			}
 			public ByReference(Pointer p) {
 				super(p);
-				// TODO Auto-generated constructor stub
-			}}
+			}
+			}
 
 		public double x;
 		public double y;
@@ -51,14 +53,28 @@ public interface FleetMasterInterfaceLib {
 	}
 		
 	public static class GridParams extends Structure {
-		public static class ByReference extends GridParams implements Structure.ByReference {}
+		public static class ByReference extends GridParams implements Structure.ByReference {
+
+			public ByReference(double origin_x, double origin_y, double origin_theta, double resolution,
+					NativeLong width, NativeLong height) {
+				super(origin_x, origin_y, origin_theta, resolution, width, height);
+			}
+			public ByReference(Pointer p) {
+				super(p);
+			}
+		}
 
 		public PathPose origin;
 		public double resolution;
 		public NativeLong width;
 		public NativeLong height;
 		
-		public GridParams() {}
+		public GridParams(double origin_x, double origin_y, double origin_theta, double resolution, NativeLong width, NativeLong height) {
+			this.origin = new PathPose(origin_x, origin_y, origin_theta);
+			this.resolution = resolution;
+			this.width = width;
+			this.height = height;
+		}
 		public GridParams(Pointer p) {
 			super(p);
 		}
@@ -69,7 +85,26 @@ public interface FleetMasterInterfaceLib {
 	}
 	
 	public static class TrajParams extends Structure {
-		public static class ByReference extends TrajParams implements Structure.ByReference {}
+		public static class ByReference extends TrajParams implements Structure.ByReference {
+
+			public ByReference(double maxVel, double maxVelRev, boolean useSteerDriveVel, double maxRotationalVel,
+					double maxRotationalVelRev, double maxSteeringAngleVel, double initVel, double endVel,
+					double initSteeringAngleVel, double endSteeringAngleVel, double maxAcc, double maxRotationalAcc,
+					double maxSteeringAngleAcc, double timeStep, double wheelBaseX, double wheelBaseY,
+					boolean useInitialState, int nbZeroVelControlCommands, double minDist,
+					boolean useCoordTimeAccConstraints, boolean useCoordTimeContraintPoints, boolean debug,
+					String debugPrefix, double creepSpeed, double creepDistance, boolean setCreepSpeedAsEndConstraint,
+					int citiTruckNbClearSpeedCommands) {
+				super(maxVel, maxVelRev, useSteerDriveVel, maxRotationalVel, maxRotationalVelRev, maxSteeringAngleVel, initVel, endVel,
+						initSteeringAngleVel, endSteeringAngleVel, maxAcc, maxRotationalAcc, maxSteeringAngleAcc, timeStep, wheelBaseX,
+						wheelBaseY, useInitialState, nbZeroVelControlCommands, minDist, useCoordTimeAccConstraints,
+						useCoordTimeContraintPoints, debug, debugPrefix, creepSpeed, creepDistance, setCreepSpeedAsEndConstraint,
+						citiTruckNbClearSpeedCommands);
+			}
+			public ByReference(Pointer p) {
+				super(p);
+			}
+		}
 
 	    double maxVel;
 	    double maxVelRev;
@@ -99,7 +134,37 @@ public interface FleetMasterInterfaceLib {
 	    boolean setCreepSpeedAsEndConstraint;
 	    int citiTruckNbClearSpeedCommands;
 		
-		public TrajParams() {}
+		public TrajParams(double maxVel, double maxVelRev, boolean useSteerDriveVel, double maxRotationalVel, double maxRotationalVelRev, double maxSteeringAngleVel, double initVel, double endVel, 
+	    double initSteeringAngleVel, double endSteeringAngleVel, double maxAcc, double maxRotationalAcc, double maxSteeringAngleAcc, double timeStep, double wheelBaseX, double wheelBaseY, boolean useInitialState,
+	    int nbZeroVelControlCommands, double minDist, boolean useCoordTimeAccConstraints, boolean useCoordTimeContraintPoints, boolean debug, String debugPrefix, double creepSpeed, double creepDistance, boolean setCreepSpeedAsEndConstraint, int citiTruckNbClearSpeedCommands) {
+		    this.maxVel = maxVel;
+		    this.maxVelRev = maxVelRev;
+		    this.useSteerDriveVel = useSteerDriveVel;
+		    this.maxRotationalVel = maxRotationalVel;
+		    this.maxRotationalVelRev = maxRotationalVelRev;
+		    this.maxSteeringAngleVel = maxSteeringAngleVel;
+		    this.initVel = initVel;
+		    this.endVel = endVel;
+		    this.initSteeringAngleVel = initSteeringAngleVel;
+		    this.endSteeringAngleVel = endSteeringAngleVel;
+		    this.maxAcc = maxAcc;
+		    this.maxRotationalAcc = maxRotationalAcc;
+		    this.maxSteeringAngleAcc = maxSteeringAngleAcc;
+		    this.timeStep = timeStep;
+		    this.wheelBaseX = wheelBaseX;
+		    this.wheelBaseY = wheelBaseY;
+		    this.useInitialState = useInitialState;
+		    this.nbZeroVelControlCommands = nbZeroVelControlCommands;
+		    this.minDist = minDist;
+		    this.useCoordTimeAccConstraints = useCoordTimeAccConstraints;
+		    this.useCoordTimeContraintPoints = useCoordTimeContraintPoints;
+		    this.debug = debug;
+		    this.debugPrefix = debugPrefix;
+		    this.creepSpeed = creepSpeed;
+		    this.creepDistance = creepDistance;
+		    this.setCreepSpeedAsEndConstraint = setCreepSpeedAsEndConstraint;
+		    this.citiTruckNbClearSpeedCommands = citiTruckNbClearSpeedCommands;
+		}
 		public TrajParams(Pointer p) {
 			super(p);
 		}
