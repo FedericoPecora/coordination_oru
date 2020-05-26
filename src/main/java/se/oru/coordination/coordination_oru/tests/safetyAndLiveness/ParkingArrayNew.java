bebe665c45f31049fe6809dac440cc5e380f2d00
Anna Mannucci <anna.mannucci90@gmail.com>
2020-05-26 15:02:10 +0200

    add save scenario/load scenario.

--- src/main/java/se/oru/coordination/coordination_oru/tests/safetyAndLiveness/ParkingArrayNew.java
+++ src/main/java/se/oru/coordination/coordination_oru/tests/safetyAndLiveness/ParkingArrayNew.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.logging.Level;
@@ -15,6 +16,7 @@ import aima.core.util.datastructure.Pair;
 import se.oru.coordination.coordination_oru.ConstantAccelerationForwardModel;
 import se.oru.coordination.coordination_oru.Mission;
 import se.oru.coordination.coordination_oru.NetworkConfiguration;
+import se.oru.coordination.coordination_oru.TrackingCallback;
 import se.oru.coordination.coordination_oru.demo.DemoDescription;
 import se.oru.coordination.coordination_oru.motionplanning.AbstractMotionPlanner;
 import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner;
@@ -50,12 +52,15 @@ public class ParkingArrayNew {
 
 		double MAX_ACCEL = 1.0;
 		double MAX_VEL = 2.5;
+		boolean CREATE_SCENARIO = false;
 		
-		//Define the radius of the circle
-		int numSlots = 8;
+		int NUMBER_ROBOTS = 20;
+		
+		//Define the parking positions
+		int numSlots = Math.max(NUMBER_ROBOTS/2, 8);
 		double deltaX = 5.0;
 		double deltaY = 15.0;
-		double offsetX = 30.0;
+		double offsetX = 15.0;
 		double offsetY = 30.0;
 				
 		//Instantiate a trajectory envelope coordinator.
@@ -71,14 +76,15 @@ public class ParkingArrayNew {
 		tec.setUseInternalCriticalPoints(false);
 		tec.setCheckEscapePoses(true);
 		tec.setYieldIfParking(false);
-		tec.setBreakDeadlocksByReordering(false);
+		tec.setBreakDeadlocksByReordering(true);
 		tec.setBreakDeadlocksByReplanning(true);
 		tec.setCheckCollisions(true);
+		tec.setUpdateAllPostedMissionsOnce(true);
 		//MetaCSPLogging.setLevel(TrajectoryEnvelopeCoordinator.class, Level.FINEST);
 		
 		//Setup the network parameters
-		NetworkConfiguration.setDelays(10, 2000);
-		NetworkConfiguration.PROBABILITY_OF_PACKET_LOSS = 0.2;
+		NetworkConfiguration.setDelays(10, 500);
+		NetworkConfiguration.PROBABILITY_OF_PACKET_LOSS = 0.;
 		tec.setNetworkParameters(NetworkConfiguration.PROBABILITY_OF_PACKET_LOSS, NetworkConfiguration.getMaximumTxDelay(), 0.02);
 				
 		//Set the footprint
@@ -91,7 +97,8 @@ public class ParkingArrayNew {
 		tec.setDefaultFootprint(footprint);
 		
 		// Set the FW models and robotIDs
-		final int[] robotIDs = new int[] {1,2,3,4,5,6,7,8,9,10,11,12};
+		final int[] robotIDs = new int[NUMBER_ROBOTS];// {1,2,3,4,5,6,7,8,9,10,11,12};
+		for (int i = 1; i <= NUMBER_ROBOTS; i++) robotIDs[i-1] = i;
 		HashSet<Integer> inactiveRobots = new HashSet<Integer>();
 //		inactiveRobots.add(1);
 //		inactiveRobots.add(2);
@@ -116,23 +123,6 @@ public class ParkingArrayNew {
 		viz.setMap(yamlFile);
 		//viz.setInitialTransform(41, -20, -25);
 		tec.setVisualization(viz);
-				
-		//Determine locations around the circle, with random orientation
-		long seed = 123123;//Calendar.getInstance().getTimeInMillis();
-		Random rand = new Random(seed);
-		ArrayList<String> locationsUp = new ArrayList<String>();
-		ArrayList<String> locationsDown = new ArrayList<String>();
-		for (int i = 0; i < numSlots; i++) {
-			locationsUp.add(i,"locUp_"+i);
-			locationsDown.add(i,"locDown_"+i);
-			Missions.setLocation(locationsUp.get(i), new Pose(offsetX+i*deltaX, offsetY+deltaY, -Math.PI/2.0));
-			Missions.setLocation(locationsDown.get(i), new Pose(offsetX+i*deltaX, offsetY, -Math.PI/2.0));
-		}
-		
-		final String statFilename = System.getProperty("user.home")+File.separator+"stats.txt";
-		String header = "Seed random generator: " + seed + "L.\n" + "#";
-		for (int robotID : robotIDs) header += (robotID + "\t");
-		initStat(statFilename, header);
 		
 		ReedsSheppCarPlanner rsp = new ReedsSheppCarPlanner();
 		rsp.setMap(yamlFile);
@@ -140,91 +130,97 @@ public class ParkingArrayNew {
 		rsp.setFootprint(footprint);
 		rsp.setTurningRadius(4.0);
 		rsp.setDistanceBetweenPathPoints(0.1);
+				
+		//Determine locations around the circle, with random orientation
+		long seed = 123123;//Calendar.getInstance().getTimeInMillis();
+		Random rand = new Random(seed);
 		
-		//Here we pre-compute all paths
-		for (int i = 0; i < numSlots; i++) {
-			for (int j = 0; j < numSlots; j++) {
-				//Compute path in both directions
-				rsp.setStart(Missions.getLocation(locationsUp.get(i)));
-				rsp.setGoals(Missions.getLocation(locationsDown.get(j)));
-				if (!rsp.doPlanning()) throw new Error("No path between " + locationsUp.get(i) + "[" + Missions.getLocation(locationsUp.get(i)) + "] and " + locationsDown.get(j) + "[" + Missions.getLocation(locationsDown.get(j)) + "]");
-				Missions.addKnownPath(locationsUp.get(i), locationsDown.get(j), rsp.getPath());
-				Missions.addKnownPath(locationsDown.get(j), locationsUp.get(i), rsp.getPathInv());
-			}			
-		}
-		
-		// Decide initial poses for robots, place the robots, and create missions
-		String[] initialLocations = new String[robotIDs.length];
-		for (int i = 0; i < robotIDs.length; i++) {
-			String upOrDown = rand.nextBoolean() ? "Up" : "Down"; 
-			String newLocation = "loc" + upOrDown + "_"+rand.nextInt(numSlots);
-			for (int j = 0; j < i; j++) {
-				if (initialLocations[j].equals(newLocation)) {
-					j = -1;
+		if (CREATE_SCENARIO) {
+			ArrayList<String> locationsUp = new ArrayList<String>();
+			ArrayList<String> locationsDown = new ArrayList<String>();
+			for (int i = 0; i < numSlots; i++) {
+				locationsUp.add(i,"locUp_"+i);
+				locationsDown.add(i,"locDown_"+i);
+				Missions.setLocation(locationsUp.get(i), new Pose(offsetX+i*deltaX, offsetY+deltaY, -Math.PI/2.0));
+				Missions.setLocation(locationsDown.get(i), new Pose(offsetX+i*deltaX, offsetY, -Math.PI/2.0));
+			}
+					
+			//Here we pre-compute all paths
+			for (int i = 0; i < numSlots; i++) {
+				for (int j = 0; j < numSlots; j++) {
+					//Compute path in both directions
+					rsp.setStart(Missions.getLocation(locationsUp.get(i)));
+					rsp.setGoals(Missions.getLocation(locationsDown.get(j)));
+					if (!rsp.doPlanning()) throw new Error("No path between " + locationsUp.get(i) + "[" + Missions.getLocation(locationsUp.get(i)) + "] and " + locationsDown.get(j) + "[" + Missions.getLocation(locationsDown.get(j)) + "]");
+					Missions.addKnownPath(locationsUp.get(i), locationsDown.get(j), rsp.getPath());
+					Missions.addKnownPath(locationsDown.get(j), locationsUp.get(i), rsp.getPathInv());
+				}			
+			}
+			
+			// Decide initial poses for robots, place the robots, and create missions
+			HashMap<Integer,String> initialLocations = new HashMap<Integer,String>();
+			for (int i = 0; i < robotIDs.length; i++) {
+				String upOrDown = rand.nextBoolean() ? "Up" : "Down"; 
+				String newLocation = "loc" + upOrDown + "_"+rand.nextInt(numSlots);
+				while (initialLocations.containsValue(newLocation)) {
 					upOrDown = rand.nextBoolean() ? "Up" : "Down"; 
 					newLocation = "loc" + upOrDown + "_"+rand.nextInt(numSlots);
 				}
+				initialLocations.put(robotIDs[i], newLocation);
 			}
-			initialLocations[i] = newLocation;
+			
+			HashSet<Pair<String,String>> assigned = new HashSet<Pair<String,String>>(); 
+			
+			//Set the goals such that two paths cannot overlap
+			for (Integer robotID : initialLocations.keySet()) {
+				String initialLocation = initialLocations.get(robotID);
+				String upOrDown = initialLocations.get(robotID).contains("Down") ? "Up" : "Down"; 
+				String goalLocation = "loc" + upOrDown + "_"+rand.nextInt(numSlots);
+				while (assigned.contains(new Pair<String,String>(goalLocation,initialLocation))) {
+					goalLocation = "loc" + upOrDown + "_"+rand.nextInt(numSlots);
+				}
+				assigned.add(new Pair<String,String>(initialLocation,goalLocation));
+				if (!inactiveRobots.contains(robotID)) {
+					Mission mFW = new Mission(robotID, Missions.getShortestPath(initialLocation, goalLocation));
+					System.out.println("Robot" + robotID + ", start: " + initialLocation + " ,goal: " + goalLocation);
+					Mission mBW = new Mission(robotID, Missions.getShortestPath(goalLocation, initialLocation));
+					Missions.enqueueMission(mFW);
+					Missions.enqueueMission(mBW);
+				}
+			}
+			Missions.saveScenario("missions/parking-array-new-" + NUMBER_ROBOTS);
 		}
+		else
+			Missions.loadScenario("missions/parking-array-new-" + NUMBER_ROBOTS);
 		
-		HashSet<Pair<String,String>> assigned = new HashSet<Pair<String,String>>(); 
 		
-		//Set the goals such that two paths cannot overlap
-		for (int i = 0; i < initialLocations.length; i++) {
-			int robotID = robotIDs[i];
+		final String statFilename = System.getProperty("user.home")+File.separator+"stats.txt";
+		String header = "Seed random generator: " + seed + "L.\n" + "#";
+		for (int robotID : robotIDs) header += (robotID + "\t");
+		initStat(statFilename, header);
+		
+		for (final int robotID : robotIDs) {
+			if (inactiveRobots.contains(robotID)) continue;
 			tec.setForwardModel(robotID, new ConstantAccelerationForwardModel(MAX_ACCEL, MAX_VEL, tec.getTemporalResolution(), tec.getControlPeriod(), tec.getTrackingPeriod()));
 			//In case deadlocks occur, we make the coordinator capable of re-planning on the fly (experimental, not working properly yet)
 			tec.setMotionPlanner(robotID, rsp.getCopy());
-			String initialLocation = initialLocations[i];
-			if (!inactiveRobots.contains(robotID)) tec.placeRobot(robotID, Missions.getLocation(initialLocation));
-			String upOrDown = initialLocation.contains("Down") ? "Up" : "Down"; 
-			String goalLocation = "loc" + upOrDown + "_"+rand.nextInt(numSlots);
-			while (initialLocation.equals(goalLocation) || assigned.contains(new Pair<String,String>(goalLocation,initialLocation))) {
-				goalLocation = "loc" + upOrDown + "_"+rand.nextInt(numSlots);
-			}
-			assigned.add(new Pair<String,String>(initialLocation,goalLocation));
-			if (!inactiveRobots.contains(robotID)) {
-			Mission mFW = new Mission(robotID, Missions.getShortestPath(initialLocation, goalLocation));
-			System.out.println("Robot" + robotID+ ", start: " + initialLocation + " ,goal: " + goalLocation);
-			Mission mBW = new Mission(robotID, Missions.getShortestPath(goalLocation, initialLocation));
-			Missions.enqueueMission(mFW);
-			Missions.enqueueMission(mBW);
-			}
+			tec.placeRobot(robotID, Missions.getInitialPoses().get(robotID));
 		}
-
+		
+		//Wait a bit (all the trackers shoud become active)
+		Thread.sleep(5000);
+		
 		for (final int robotID : robotIDs) {
-			if (!inactiveRobots.contains(robotID)) {
-				tec.setMotionPlanner(robotID,rsp);
-				Thread t = new Thread() {
-					public void run() {
-//						while (true) {
-//							synchronized(tec.getSolver()) {
-//								if (tec.isFree(robotID)) {
-//									Mission m = Missions.dequeueMission(robotID);
-//									tec.addMissions(m);
-//									tec.computeCriticalSectionsAndStartTrackingAddedMission();
-//									Missions.enqueueMission(m);
-//								}
-//							}
-//							try { Thread.sleep(2000); }
-//							catch (InterruptedException e) { e.printStackTrace(); }
-//						}
-						boolean firstTime = true;
-						int totalIterations = 12;
-						//if (robotID%2 == 0) totalIterations = 19;
-						boolean isFree = false;
-						long startTime = Calendar.getInstance().getTimeInMillis();
-						while (true && totalIterations > 0) {
+			if (inactiveRobots.contains(robotID)) continue;
+			Thread t = new Thread() {
+				public void run() {
+					boolean firstTime = true;
+					int totalIterations = 12;
+					long startTime = Calendar.getInstance().getTimeInMillis();
+					
+					while (true && totalIterations > 0) {
 							synchronized(tec.getSolver()) {
-								if (tec.isFree(robotID)) isFree = true;
-							}
-							//Sleep for a little
-							try { Thread.sleep(2000); }
-							catch (InterruptedException e) { e.printStackTrace(); }
-							if (isFree) {
-								isFree = false;
-								synchronized(tec.getSolver()) {
+								if (tec.isFree(robotID)) {
 									if (!firstTime) {
 										long elapsed = Calendar.getInstance().getTimeInMillis()-startTime;
 										String stat = "";
@@ -234,24 +230,25 @@ public class ParkingArrayNew {
 									}
 									startTime = Calendar.getInstance().getTimeInMillis();
 									firstTime = false;
-									Mission m = Missions.dequeueMission(robotID);
-									tec.addMissions(m);
-									Missions.enqueueMission(m);
-									totalIterations--;
-								}
+									
+									Mission m = Missions.peekMission(robotID);
+									if (tec.addMissions(m)) {
+										Missions.dequeueMission(robotID);
+										Missions.enqueueMission(m);
+										totalIterations--;
+									}
 							}
-							//Sleep for a little
-							try { Thread.sleep(2000); }
-							catch (InterruptedException e) { e.printStackTrace(); }
 						}
-						System.out.println("Robot" + robotID + " is done!");
-						
+						//Sleep for a little
+						try { Thread.sleep(2000); }
+						catch (InterruptedException e) { e.printStackTrace(); }
 					}
-				};
-				t.start();
-				try { Thread.sleep(1000); }
-				catch (InterruptedException e) { e.printStackTrace(); }
-			}
+					System.out.println("Robot" + robotID + " is done!");
+				}
+			};
+			t.start();
+			try { Thread.sleep(1000); }
+			catch (InterruptedException e) { e.printStackTrace(); }
 		}
 		
 	}
