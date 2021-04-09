a1c9039bcdec623b72203d8897eb818f8ece5b23
Chittaranjan S Srinivas <ksatyaki@gmail.com>
2021-04-09 11:29:10 +0200

    exp

--- src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments_all.sh
+++ src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export P_TIME="t1"
-export S_TYPE="corridor"
-mkdir journal_logs/corridor-t1
+export S_TYPE="corridor1"
+mkdir journal_logs/corridor1-t1
 ./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
 echo "*********************************"
 echo "*********************************"
@@ -11,7 +11,7 @@ echo "*********************************"
 
 export P_TIME="t2"
 export S_TYPE="corridor"
-mkdir journal_logs/corridor-t2
+mkdir journal_logs/corridor1-t2
 ./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
 echo "*********************************"
 echo "*********************************"
@@ -21,7 +21,7 @@ echo "*********************************"
 
 export P_TIME="t3"
 export S_TYPE="corridor"
-mkdir journal_logs/corridor-t3
+mkdir journal_logs/corridor1-t3
 ./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
 echo "*********************************"
 echo "*********************************"
@@ -82,6 +82,37 @@ echo "*********************************"
 export P_TIME="t3"
 export S_TYPE="corridor3"
 mkdir journal_logs/corridor3-t3
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+
+export P_TIME="t1"
+export S_TYPE="corridor4"
+mkdir journal_logs/corridor4-t1
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t2"
+export S_TYPE="corridor4"
+mkdir journal_logs/corridor4-t2
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t3"
+export S_TYPE="corridor4"
+mkdir journal_logs/corridor4-t3
 ./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
 echo "*********************************"
 echo "*********************************"
