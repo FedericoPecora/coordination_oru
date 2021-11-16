3d0f39abdc073cc43fc68e7cf25cb894f166797b
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2021-11-16 20:01:15 +0100

    Gitignore chitttests

---
+++ src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments_ellipse.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+export P_TIME="t2"
+export S_TYPE="scenario3"
+mkdir journal_logs/scenario3-t2
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t3"
+export S_TYPE="scenario3"
+mkdir journal_logs/scenario3-t3
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t4"
+export S_TYPE="scenario3"
+mkdir journal_logs/scenario3-t4
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t5"
+export S_TYPE="scenario3"
+mkdir journal_logs/scenario3-t5
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t6"
+export S_TYPE="scenario3"
+mkdir journal_logs/scenario3-t6
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t7"
+export S_TYPE="scenario3"
+mkdir journal_logs/scenario3-t7
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+
+export P_TIME="t2"
+export S_TYPE="scenario4"
+mkdir journal_logs/scenario4-t2
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t3"
+export S_TYPE="scenario4"
+mkdir journal_logs/scenario4-t3
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t4"
+export S_TYPE="scenario4"
+mkdir journal_logs/scenario4-t4
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t5"
+export S_TYPE="scenario4"
+mkdir journal_logs/scenario4-t5
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t6"
+export S_TYPE="scenario4"
+mkdir journal_logs/scenario4-t6
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
+
+export P_TIME="t7"
+export S_TYPE="scenario4"
+mkdir journal_logs/scenario4-t7
+./src/main/java/se/oru/coordination/coordination_oru/tests/testsPedestrians/start_experiments.sh
+echo "*********************************"
+echo "*********************************"
+echo "************* DONE **************"
+echo "*********************************"
+echo "*********************************"
